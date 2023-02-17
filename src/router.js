e577added67a821939e673baecfa8356bcc69739
aldiramdan <aldi.ramdan96@gmail.com>
2023-02-17 23:46:29 +0700

    add login

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/index'
+import Login from './pages/login/index'
 
 function router() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     </BrowserRouter>
   )
