2cc22789e9745ef02fc73a98923de56b03a6f9af
aldiramdan <aldi.ramdan96@gmail.com>
2023-02-19 19:44:29 +0700

    add vehicle list

---
+++ src/pages/vehicle/index.jsx
@@ -0,0 +1,172 @@
+/* eslint-disable array-callback-return */
+import { React, useEffect, useState } from 'react'
+import Navbar from '../../components/navbar'
+import Footer from '../../components/footer'
+import Card from '../../components/card'
+import axios from 'axios'
+
+function Vehicle() {
+	const [vehicle, setVehicle] = useState([])
+
+	const getPopularVehicle = async () => {
+		try {
+			const { data } = await axios.get(
+			`http://localhost:3080/vehicle/popular`
+			)
+			setVehicle(data.data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+    useEffect(() => {
+        getPopularVehicle()
+    })
+
+    return (
+        <div className="App">
+            <Navbar />
+
+            <section className="popular-in-town">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <h2 className="title">Popular in Town</h2>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="d-flex justify-content-end">
+								<div className="view-all">
+									<p className="text-end fw-bold">
+										{'View all '}
+										<b>{'>'}</b>
+									</p>
+								</div>
+							 </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        {vehicle.map((v, k) => {
+							if (k < 4) {
+								return (
+								<Card
+									id={v.vehicle_id}
+									picture={`http://localhost:3080/`+v.picture}
+									name={v.name}
+									location={v.location}
+								/>
+							)}
+						})}
+                    </div>
+                </div>
+            </section>
+
+            <section className="popular-in-town">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <h2 className="title">Cars</h2>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="d-flex justify-content-end">
+                                <div className="view-all">
+                                    <p className="text-end fw-bold">
+                                        {'View all '}
+                                        <b>{'>'}</b>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="row">
+                        {vehicle.map((v, k) => {
+                            if (k < 4) {
+                                return (
+                                <Card
+                                    id={v.vehicle_id}
+                                    picture={`http://localhost:3080/`+v.picture}
+                                    name={v.name}
+                                    location={v.location}
+                                />
+                            )}
+                        })}
+                    </div>
+                </div>
+            </section>
+
+            <section className="popular-in-town">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <h2 className="title">Motocycle</h2>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="d-flex justify-content-end">
+                                <div className="view-all">
+                                    <p className="text-end fw-bold">
+                                        {'View all '}
+                                        <b>{'>'}</b>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        {vehicle.map((v, k) => {
+                            if (k < 4) {
+                                return (
+                                <Card
+                                    id={v.vehicle_id}
+                                    picture={`http://localhost:3080/`+v.picture}
+                                    name={v.name}
+                                    location={v.location}
+                                />
+                            )}
+                        })}
+                    </div>
+                </div>
+            </section>
+
+            <section className="popular-in-town">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <h2 className="title">Bike</h2>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="d-flex justify-content-end">
+                                <div className="view-all">
+                                    <p className="text-end fw-bold">
+                                        {'View all '}
+                                        <b>{'>'}</b>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        {vehicle.map((v, k) => {
+                            if (k < 4) {
+                                return (
+                                <Card
+                                    id={v.vehicle_id}
+                                    picture={`http://localhost:3080/`+v.picture}
+                                    name={v.name}
+                                    location={v.location}
+                                />
+                            )}
+                        })}
+                    </div>
+                </div>
+            </section>
+
+            <Footer />
+        </div>
+    )
+    
+}
+
+export default Vehicle
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/index'
 import Login from './pages/login/index'
 import Register from './pages/register/index'
+import Vehicle from './pages/vehicle/index'
 
 function router() {
   return (
@@ -11,6 +12,7 @@ function router() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/vehicle" element={<Vehicle />} />
         </Routes>
     </BrowserRouter>
   )
