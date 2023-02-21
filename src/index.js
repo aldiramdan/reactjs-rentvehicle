3b0a3c4b13185f081700f32a66b4a596c3c237c3
aldiramdan <aldi.ramdan96@gmail.com>
2023-02-21 10:00:42 +0700

    add router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import App from './router';
+import App from './routers/router';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home/index'
-import Login from './pages/login/index'
-import Register from './pages/register/index'
-import Vehicle from './pages/vehicle/index'
+import Home from '../pages/home/index'
+import Login from '../pages/login/index'
+import Register from '../pages/register/index'
+import Vehicle from '../pages/vehicle/index'
 
 function router() {
   return (
