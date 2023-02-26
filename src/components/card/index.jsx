4a333da21c94c85cb2081ce1478bea4552ca314d
aldiramdan <aldi.ramdan96@gmail.com>
2023-02-26 09:41:12 +0700

    fixed

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,16 +1,22 @@
 import { React } from 'react'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 function Card(props) {
 	return (
 		<div className="col-md-3">
-			<div className="card card-popular">
-				<img src={props.picture} className="card-img-top card-image" alt="..."></img>
-			</div>
-			<div className="card-content">
-				<h5 className="card-title">{props.name}</h5>
-				<p className="card-text">{props.location}</p>
-			</div>
+			 <Link to={'/vehicle/detail/' + props.id} className="link-card">
+				<div className="card card-popular">
+					<img src={props.picture} className="card-img-top card-image" alt="..."></img>
+				</div>
+				<div className="card-rating">
+					<p>{props.rating}</p>
+				</div>
+				<div className="card-content">
+					<h5 className="card-title">{props.name}</h5>
+					<p className="card-text">{props.location}</p>
+				</div>
+			 </Link>
 		</div>
 	)
 }
