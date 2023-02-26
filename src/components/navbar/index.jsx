1e92a89711da68c2fa206ba7f3b8577251c9b739
aldiramdan <aldi.ramdan96@gmail.com>
2023-02-26 09:41:45 +0700

    fixed

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,39 @@
-import { React } from 'react'
+import { React, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../../stores/reducer/users'
+import useApi from '../../helpers/useApi'
 import logo from '../../img/logo.png'
 
 import './style.css'
 
 function Navbar(props) {
+    const dispatch = useDispatch()
+    const { isAuth } = useSelector((state) => state.users)
+    const [user, setUser] = useState({})
+    const api = useApi()
+
+    const getUser = () => {
+            api
+            .request({
+                method: 'GET',
+                url: 'users/profile'
+        })
+        .then((res) => {
+            const { data } = res.data
+            setUser(data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    console.log(user);
+
     return (
         <div className="container-fluid">
             <header className="py-3">
@@ -30,26 +59,55 @@ function Navbar(props) {
                             <Link to="/vehicle" className="nav-link navmenu">
                                 Vehicle Type
                             </Link>
-                            <Link to="/history" className="nav-link navmenu">
-                                History
-                            </Link>
-                            <Link to="/about" className="nav-link navmenu">
-                                About
-                            </Link>            
-                            <Link to="/login">
-                                <button
-                                    type="button"
-                                    className="btn btn-outline-warning rounded-3 mx-2 my-2 navbtn">
-                                    Login
-                                </button>
-                            </Link>
-                            <Link to="/register">
-                                <button
-                                    type="button"
-                                    className="btn btn-warning rounded-3 mx-2 my-2 navbtn">
-                                    Register
-                                </button>
-                            </Link>
+                            {isAuth ? (
+                                <>
+                                <Link to="/history" className="nav-link navmenu">
+                                    History
+                                </Link>
+                                <Link to="/about" className="nav-link navmenu">
+                                    About
+                                </Link>
+                                <div className="dropdown">
+                                        <button class="btn btn-transparent dropdown-toggle"
+                                            type="button"
+                                            id="dropdownMenuButton"
+                                            data-toggle="dropdown"
+                                            aria-haspopup="true"
+                                            aria-expanded="false">
+                                            <img src={`${process.env.REACT_APP_BASE_URL}` + user.picture} className="profile-pic rounded-circle" alt="" />
+                                        </button>
+                                        <div className="dropdown-menu dropdown-menu-right">
+                                            <h6 class="dropdown-header">{user.name}</h6>
+                                            <Link to="/dashboard" className="dropdown-item">
+                                                Dashboard
+                                            </Link>
+                                            <Link to="/" className="dropdown-item" onClick={() => dispatch(logout())}>
+                                                Logout
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </>
+                                ) : (
+                                <>
+                                    <Link to="/about" className="nav-link navmenu">
+                                        About
+                                    </Link>
+                                    <Link to="/login">
+                                        <button
+                                            type="button"
+                                            className="btn btn-outline-warning rounded-3 mx-2 my-2 navbtn">
+                                            Login
+                                        </button>
+                                    </Link>
+                                    <Link to="/register">
+                                        <button
+                                            type="button"
+                                            className="btn btn-warning rounded-3 mx-2 my-2 navbtn">
+                                            Register
+                                        </button>
+                                    </Link>
+                                </>            
+                             )}
                         </div>
                     </div>
                 </nav>
