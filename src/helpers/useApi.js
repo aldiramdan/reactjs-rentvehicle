faad4a1e3088abebecf075c46ba05adc312edbc2
aldiramdan <aldi.ramdan96@gmail.com>
2023-02-26 09:44:00 +0700

    fixed

--- src/helpers/useApi.js
+++ src/helpers/useApi.js
@@ -1,6 +1,6 @@
-import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import axios from 'axios'
 
 function useApi (urls = '') {
     const { token } = useSelector((state) => state.users)
--- src/helpers/withAuth.js
+++ src/helpers/withAuth.js
@@ -1,13 +1,17 @@
 import { Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
-const withAuth = (Component) => {
+const withAuth = (Component, roles) => {
     return (props) => {
         const { isAuth, data } = useSelector((state) => state.users)
 
         if (!isAuth) {
             return <Navigate to="/login" />
         }
+
+        if (roles !== data.role) {
+            return <Navigate to="/" />
+      }
 
          return <Component {...props} />
     }
